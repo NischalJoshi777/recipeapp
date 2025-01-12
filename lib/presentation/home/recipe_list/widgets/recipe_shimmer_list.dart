a869b25e349ccdcc5fba7d46f6eda0c7681fbd8f
Nischal Joshi <nischaljoshi77@gmail.com>
2025-01-12 12:46:27 -0600

    feat: Updated shimmer effect for recipe list.

---
+++ lib/presentation/home/recipe_list/widgets/recipe_shimmer_list.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:myrecipeapp/config/theme/color.dart';
+import 'package:shimmer/shimmer.dart';
+
+class RecipeShimmerList extends StatelessWidget {
+  const RecipeShimmerList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: 10,
+      separatorBuilder: (BuildContext context, int index) =>
+          const SizedBox(width: 12.0),
+      itemBuilder: (context, index) => const RecipeShimmerItem(),
+    );
+  }
+}
+
+class RecipeShimmerItem extends StatelessWidget {
+  const RecipeShimmerItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 8.0,
+      children: [
+        ShimmerItem(
+          width: 160.0,
+          height: 120.0,
+        ),
+        ShimmerItem(
+          width: 160.0,
+          height: 12.0,
+        ),
+        ShimmerItem(
+          width: 120.0,
+          height: 12.0,
+        ),
+      ],
+    );
+  }
+}
+
+class ShimmerItem extends StatelessWidget {
+  final double width;
+  final double height;
+
+  const ShimmerItem({
+    super.key,
+    required this.width,
+    required this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const color = Palette.darkGray;
+    return Shimmer.fromColors(
+      baseColor: Colors.grey,
+      highlightColor: color.withValues(alpha: 0.1),
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: color..withValues(alpha: 0.25),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+    );
+  }
+}
